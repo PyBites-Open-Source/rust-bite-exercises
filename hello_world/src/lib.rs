b8ac05808e55cba133e2aec221d06bf2185f78ec
tanner-ray-martin <tanner.ray.martin.123@gmail.com>
2024-09-03 08:30:14 -0500

    new exercise Hello, World!

---
+++ hello_world/src/lib.rs
@@ -0,0 +1,13 @@
+fn hello_world() -> String {
+    "Hello, World!".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello_world() {
+        assert_eq!(hello_world(), "Hello, World!");
+    }
+}
